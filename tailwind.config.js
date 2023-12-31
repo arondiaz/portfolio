fafe27ce5d5358f88976a31e0b2753657e597695
Arón Díaz <arondiaz.cpn@gmail.com>
2023-12-31 15:02:59 -0300

    added portfolio finishe

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/components/AboutMe.jsx",
+  ],
+
+  theme: {
+    extend: {
+      screens: {
+        xs: "400px",
+      },
+      animation: {
+        "loop-scroll": "loop-scroll 50s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+        backgroundImage: {
+          "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+          "gradient-conic":
+            "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        },
+      },
+      colors: {
+        primary: "#a1a1aa",
+        // primary: "#AEB6BF",
+        whitef: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
